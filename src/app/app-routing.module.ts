9286e850b4d61fa9643dafac83f56200783dd106
crislainecsoares <crislaines@dimed.com.br>
2021-08-11 14:11:59 -0300

    Exibição de categorias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import { HomeComponent } from './components/views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path : 'categorias',
+    component: CategoriaReadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
