9184fe206e6229f03c30949ce455c05a02f485a3
crislainecsoares <crislaines@dimed.com.br>
2021-08-11 14:50:21 -0300

    Ajustes

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Categoria } from './categoria.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +18,6 @@ export class CategoriaService {
     const url = `${this.baseUrl}/categorias`
     return this.http.get<Categoria[]>(url)
   }
+
+
 }
